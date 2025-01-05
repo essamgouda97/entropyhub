eedd8d701729251b6cb5a29a5bfea9cb46d3a37d
YosifElessawi <yosifelessawi@yahoo.com>
2025-01-05 02:59:41 -0500

    initialize project structure

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl font-bold'>HomePage</h1>
+      
+    </div>
+  )
+}
+
+export default Home
